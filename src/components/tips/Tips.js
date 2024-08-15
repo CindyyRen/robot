14a0e908bf34522152c95a0dd28a0c302c3110d0
Cindy Ren <bdcindy78@gmail.com>
2024-08-16 08:57:11 +1000

    tips

--- src/components/tips/Tips.js
+++ src/components/tips/Tips.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { MdKeyboardDoubleArrowRight } from 'react-icons/md';
+import { facing } from '../../utils/constants';
+import './tips.css'
 
 const Tips = () => {
-  return <div>Tips</div>;
+  return (
+    <div className="tips">
+      <h4>Tips: </h4>
+      <ul>
+        <li>
+          <MdKeyboardDoubleArrowRight /> Commands is{' '}
+          <strong>CASE INSENSITIVE</strong>;{' '}
+        </li>
+        <li>
+          <MdKeyboardDoubleArrowRight /> Invalid commands will be discarded;{' '}
+        </li>
+        <li>
+          <MdKeyboardDoubleArrowRight /> Facing value :
+          <strong>{facing.map((face, index) => `"${face}",`)}</strong>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Tips;
