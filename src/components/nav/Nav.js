713c30039e1de415ef29299313ee55d4d5e3f3be
Cindy Ren <bdcindy78@gmail.com>
2024-08-15 13:57:09 +1000

    router and nav

---
+++ src/components/nav/Nav.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './nav.css';
+import bellroy from '../../assets/bellroy-white.png'
+
+const Nav = () => {
+  return (
+    <nav className="nav">
+      <Link to="/" className="logo">
+        <img src={bellroy} alt="robot" width="60px" height="29px" />
+      </Link>
+      <ul>
+        <li></li>
+        <li>
+          <Link to="/">home</Link>
+        </li>
+        <li>
+          <Link to="/robot">robot</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
---
+++ src/components/robotController/RobotController.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RobotController = () => {
+  return (
+    <div>RobotController</div>
+  )
+}
+
+export default RobotController
